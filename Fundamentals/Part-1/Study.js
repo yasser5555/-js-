e5aa63364a3ed41f33cf6f2656e5e7732f29097e
mohamed-yasser <48107732+yasser5555@users.noreply.github.com>
2024-10-06 13:01:18 +0300

    Add files via upload

---
+++ Fundamentals/Part-1/Study.js
@@ -0,0 +1,34 @@
+// let name = data 
+let name = "yasser";
+const numbers = 12345678910;
+console.log(name);
+console.log(`my name is "${name}"`);
+name = "ahmed"; // changed value to ahmed by assigment-operator
+console.log(`my name is "${name}"`);
+console.log(`my number is "${numbers}"`);
+let _null= null;
+console.log(typeof _null);
+let Number = "1000000000"; //
+console.log(Number);
+// by using urnary condition
+console.log(+Number);
+console.log(1/2); // divisions
+console.log(1-2); // substracting
+console.log(1+2); // adding
+console.log(1*2); // multpilying
+console.log(1%2); // Reminder of division
+let  i = 2;
+console.log(i); // Before incrementing
+i++
+console.log(i); // After increment
+console.log(i>1); // is i more than  1
+console.log(i<1); // is i less than  1
+console.log(i==1); // is i equal to 1 ? 
+console.log(i<=1); // is i less than or equal to 1 ? 
+console.log(i==1); // is i equal to 1 in value ? 
+console.log(i===1); // is i equal to 1 in value & Data-type ? 
+console.log(i>=1); // is i more than or equal to 1 ? 
+// null-safety 
+// Syntax: variable = value || backup-value
+let n1 = null ?? 2;
+console.log(n1);
